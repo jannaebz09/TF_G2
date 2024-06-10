c9bb86a42b1f000ff18ca9675e64275c7ca8a119
Gianpiero <gianpierojesus1309@gmail.com>
2024-06-09 22:17:31 -0500

    Parte de Gianpiero

--- src/app/components/expcertificate/listarexpcertificate/listarexpcertificate.component.ts
+++ src/app/components/expcertificate/listarexpcertificate/listarexpcertificate.component.ts
@@ -4,30 +4,37 @@ import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { ExpCertificate } from '../../../models/ExpCertificate';
 import { ExpcertificateService } from '../../../services/expcertificate.service';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-listarexpcertificate',
   standalone: true,
-  imports: [MatTableModule, MatFormFieldModule, MatPaginatorModule],
+  imports: [MatTableModule, MatFormFieldModule, MatPaginatorModule,MatIconModule,RouterLink],
   templateUrl: './listarexpcertificate.component.html',
   styleUrl: './listarexpcertificate.component.css'
 })
-export class ListarexpcertificateComponent implements OnInit,AfterViewInit{
-  displayedColumns: string[] = ['c1', 'c2', 'c3','c4','c5'];
+export class ListarexpcertificateComponent implements OnInit{
+  displayedColumns: string[] = ['c1', 'c2', 'c3','c4','c5','c6','c7'];
   dataSource: MatTableDataSource<ExpCertificate> = new MatTableDataSource();
   constructor(private eS: ExpcertificateService) {}
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-  }
   ngOnInit(): void {
     this.eS.list().subscribe((data) => {
       this.dataSource = new MatTableDataSource(data);
-
+      this.dataSource.paginator = this.paginator;
     });
     this.eS.getList().subscribe((data)=>{
       this.dataSource=new MatTableDataSource(data)
+      this.dataSource.paginator = this.paginator;
     })
+  }
+  eliminar(id: number) {
+    this.eS.eliminar(id).subscribe((data) => {
+      this.eS.list().subscribe((data) => {
+        this.eS.setList(data);
+      });
+    });
   }
 }
