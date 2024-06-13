cfe5ae8eb1d3735972fa69875a70d9137852be10
GianpieroFT <gianpierojesus1309@gmail.com>
2024-06-12 22:27:15 -0500

    Parte de Gianpiero

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,31 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
+export function tokenGetter() {
+  return sessionStorage.getItem('token');
+}
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
-    provideClientHydration(),
+    provideHttpClient(withInterceptorsFromDi(),withFetch()),
+    importProvidersFrom(
+      JwtModule.forRoot({
+        config: {
+          tokenGetter: tokenGetter,
+          allowedDomains: ['localhost:8082'],
+          disallowedRoutes: ['http://localhost:8082/login/forget'],
+        },
+      })
+    ),
     provideAnimationsAsync(),
-    provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
   ],
 };
