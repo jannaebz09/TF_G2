f36bcaa3298c19f2a3e896f339e0482d80be76c0
AntonioP167 <aperezpacora@hotmail.com>
2024-06-21 18:03:39 -0500

    parte de antonio

--- src/app/components/reports/report05/report05.component.ts
+++ src/app/components/reports/report05/report05.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommentService } from '../../../services/comment.service';
+import { NgFor } from '@angular/common';
+import { Comment } from '../../../models/Comment';
+
 
 @Component({
   selector: 'app-report05',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './report05.component.html',
-  styleUrl: './report05.component.css'
+  styleUrls: ['./report05.component.css']
 })
-export class Report05Component {
+export class Report05Component implements OnInit {
+  comments: Comment[] = [];
 
+  constructor(private commentService: CommentService) {}
+
+  ngOnInit(): void {
+    this.commentService.getCommentsGreaterThan3().subscribe(
+      (data) => {
+        this.comments = data;
+      },
+      (error) => {
+        console.error('Error fetching comments', error);
+      }
+    );
+  }
 }
+
--- src/app/services/optionpay.service.ts
+++ src/app/services/optionpay.service.ts
@@ -3,34 +3,47 @@ import { environment } from '../environments/environment';
 import { Subject } from 'rxjs';
 import { OptionPay } from '../models/OptionPay';
 import { HttpClient } from '@angular/common/http';
+
 const base_url = environment.base;
+
 @Injectable({
   providedIn: 'root'
 })
 export class OptionPayService {
   private url = `${base_url}/OptionPays`;
   private listaCambio = new Subject<OptionPay[]>();
+
   constructor(private httpClient: HttpClient) {}
+
   list() {
     return this.httpClient.get<OptionPay[]>(this.url);
   }
+
   insert(o: OptionPay) {
     return this.httpClient.post(this.url, o);
   }
+
   setList(listaNueva: OptionPay[]) {
     this.listaCambio.next(listaNueva);
   }
+
   getList() {
     return this.listaCambio.asObservable();
   }
+
   listId(id: number) {
     return this.httpClient.get<OptionPay>(`${this.url}/${id}`);
   }
+
   update(ou: OptionPay) {
     return this.httpClient.put(this.url, ou);
   }
 
   eliminar(id: number) {
     return this.httpClient.delete(`${this.url}/${id}`);
+  }
+
+  getlistByCard() {
+    return this.httpClient.get<OptionPay[]>(`${this.url}/listarportarjeta`);
   }
 }
